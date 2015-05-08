5dfd38e8f6447df3f438e3f3d58182d6a0516969
Arie Prasetyo <aprasetyo@traveloka.com>
2015-05-08 21:49:51 +0700

    Initial Commit

---
+++ Shoot Your Dream/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shoot Your Dream
+//
+//  Created by Arie Prasetyo on 5/8/15.
+//  Copyright (c) 2015 Arie Prasetyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shoot Your DreamTests/Shoot_Your_DreamTests.m
@@ -0,0 +1,40 @@
+//
+//  Shoot_Your_DreamTests.m
+//  Shoot Your DreamTests
+//
+//  Created by Arie Prasetyo on 5/8/15.
+//  Copyright (c) 2015 Arie Prasetyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Shoot_Your_DreamTests : XCTestCase
+
+@end
+
+@implementation Shoot_Your_DreamTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
